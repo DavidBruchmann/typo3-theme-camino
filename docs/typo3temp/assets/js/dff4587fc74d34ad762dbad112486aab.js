4b134a0e822fb99ab589597bb9077250331b0cc5
David Bruchmann <david.bruchmann@gmail.com>
2026-04-14 08:44:03 +0700

    [BUGFIX] update links in static website
    The script to download the TYPO3 page and to adjust links
    has been updated, so the resulting links are expected to work now.
    The script is not included in this extension.
    
    No changes in the code have been made.

---
+++ docs/typo3temp/assets/js/dff4587fc74d34ad762dbad112486aab.js
@@ -0,0 +1,59 @@
+(function() {
+    const subNavLinks = document.querySelectorAll('ul#subnavigation-4 a');
+    
+    // 1. Function to apply the theme
+    function applyTheme (themeName) {
+        document.body.className = document.body.className.replace(/\btheme-\S+/g, '').trim();
+        document.body.classList.add(themeName);
+        localStorage.setItem('camino-theme', themeName);
+    };
+
+    // 2. Check for saved theme on page load
+    const savedTheme = localStorage.getItem('camino-theme');
+    if (savedTheme) {
+        applyTheme(savedTheme);
+    }
+
+    // 3. Attach event listeners
+    subNavLinks.forEach(link => {
+       if (link.attributes.href.value.indexOf('theme-color-') > -1 || link.attributes.href.value.indexOf('theme-farbe-') > -1) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const themeName = 'theme-' + this.textContent.trim().toLowerCase().replace(/\s+/g, '-');
+            applyTheme(themeName);
+        });
+      }
+    });
+})();
+/*
+(function() {
+    const subNavLinks = document.querySelectorAll('ul#subnavigation-4 a');
+
+    console.log(subNavLinks);
+    
+    // 1. Function to apply the theme
+    const applyTheme = (themeName) => {
+        document.body.className = document.body.className.replace(/\btheme-\S+/g, '').trim();
+        document.body.classList.add(themeName);
+        localStorage.setItem('camino-theme', themeName);
+    };
+
+    // 2. Check for saved theme on page load
+    const savedTheme = localStorage.getItem('camino-theme');
+    if (savedTheme) {
+        applyTheme(savedTheme);
+    }
+
+    // 3. Attach event listeners
+    subNavLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            if (link.attributes.href.indexof('theme-color-') > -1) {
+              e.preventDefault();
+              const themeName = 'theme-' + this.textContent.trim().toLowerCase().replace(/\s+/g, '-');
+              applyTheme(themeName);
+            }
+        });
+    });
+})();
+*/
+      
